d0c943094d74b2c379797a6fddbfe3f12debe975
Jeegna Patel <jeegnathebug@users.noreply.github.com>
2016-03-10 12:21:13 -0500

    Added method to choose which shape is deployed

--- Tetris/TetrisLibrary/IShapeFactory.cs
+++ Tetris/TetrisLibrary/IShapeFactory.cs
@@ -6,5 +6,10 @@
         /// Deploys a new <see cref="Shape"/> instance.
         /// </summary>
         void DeployNewShape();
+        
+        /// <summary>
+        /// Deploys a new <see cref="Shape"/> instance with a given number.
+        /// </summary>
+        void DeployShape(int num);
     }
 }
