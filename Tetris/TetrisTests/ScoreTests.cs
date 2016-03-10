cc868356c9fbeca48e2ef48f4789348a273888bf
Jeegna Patel <jeegnathebug@users.noreply.github.com>
2016-03-10 11:54:02 -0500

    Added test cases to Score

--- Tetris/TetrisTests/ScoreTests.cs
+++ Tetris/TetrisTests/ScoreTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TetrisLibrary;
 
@@ -8,39 +8,152 @@ namespace TetrisTests
     public class ScoreTests
     { 
         [TestMethod]
-        public void Score()
+        public void Score_100()
         {
             //arrange
-            Board board = new Board();
-            Score score = new Score(board);
-
-            ScoreTests matan = new ScoreTests();
-            //act -> maybe just see if event flag is raised
-            board.LinesCleared += new LinesClearedHandler(Board_LinesCleared);
-            Board_LinesCleared(2);
-            
-            //assert
-            Assert.AreEqual(0, score.Lines);
-            Assert.AreEqual(1, score.Level);
-            Assert.AreEqual(0, score.score);
-        }
-        [TestMethod]
-        public void GameOverTest()
-        {
-            //arrange
-            Board board = new Board();
-            Score score = new Score(board);
-            bool eventCaught = false;
+			Board board = new Board();
+			IShapeFactory shapeFactory = board.ShapeFactory;
+			IShape shape;
+			Score score = new Score(board);
 
             //act
-            board.GameOver += () => eventCaught = true;
-            //assert
-            Assert.IsTrue(eventCaught);
+			shapeFactory.DeployShape(3);
+			shape = board.Shape;
+			shape.MoveLeft();
+			shape.MoveLeft();
+			shape.MoveLeft();
+			shape.MoveLeft();
+			shape.Drop();
+
+			shapeFactory.DeployShape(3);
+			shape = board.Shape();
+			shape.MoveLeft();
+			shape.MoveLeft();
+			shape.Drop();
+
+			shapeFactory.DeployShape(3);
+			shape = board.Shape();
+			shape.Drop();
+
+			shapeFactory.DeployShape(3);
+			shape = board.Shape();
+			shape.MoveRight();
+			shape.MoveRight();
+			shape.Drop();
+
+			shapeFactory.DeployShape(3);
+			shape = board.Shape();
+			shape.MoveRight();
+			shape.MoveRight();
+			shape.MoveRight();
+			shape.MoveRight();
+			shape.Drop();
+
+			//assert			
+			Assert.AreEqual(200, score.score);
+			Assert.AreEqual(2, score.Lines);
+			Assert.AreEqual(2, score.Level);
         }
 
-        private void Board_LinesCleared(int num)
-        {
-            Console.WriteLine("cleared "+ num);
-        }
+		[TestMethod]
+		public void Score_400()
+		{
+			//arrange
+			Board board = new Board();
+			IShapeFactory shapeFactory = board.ShapeFactory;
+			IShape shape;
+			Score score = new Score(board);
+
+			//act
+			shapeFactory.DeployShape(0);
+			shape = board.Shape;
+			shape.MoveDown();
+			shape.Rotate();
+			shape.MoveLeft();
+			shape.MoveLeft();
+			shape.MoveLeft();
+			shape.MoveLeft();
+			shape.MoveLeft();
+			shape.Drop();
+
+			shapeFactory.DeployShape(0);
+			shape = board.Shape;
+			shape.MoveDown();
+			shape.Rotate();
+			shape.MoveLeft();
+			shape.MoveLeft();
+			shape.MoveLeft();
+			shape.MoveLeft();
+			shape.Drop();
+
+			shapeFactory.DeployShape(0);
+			shape = board.Shape;
+			shape.MoveDown();
+			shape.Rotate();
+			shape.MoveLeft();
+			shape.MoveLeft();
+			shape.MoveLeft();
+			shape.Drop();
+
+			shapeFactory.DeployShape(0);
+			shape = board.Shape;
+			shape.MoveDown();
+			shape.Rotate();
+			shape.MoveLeft();
+			shape.MoveLeft();
+			shape.Drop();
+
+			shapeFactory.DeployShape(0);
+			shape = board.Shape;
+			shape.MoveDown();
+			shape.Rotate();
+			shape.MoveLeft();
+			shape.Drop();
+
+			shapeFactory.DeployShape(0);
+			shape = board.Shape;
+			shape.MoveDown();
+			shape.Rotate();
+			shape.Drop();
+
+			shapeFactory.DeployShape(0);
+			shape = board.Shape;
+			shape.MoveDown();
+			shape.Rotate();
+			shape.MoveRight();
+			shape.Drop();
+
+			shapeFactory.DeployShape(0);
+			shape = board.Shape;
+			shape.MoveDown();
+			shape.Rotate();
+			shape.MoveRight();
+			shape.MoveRight();
+			shape.Drop();
+
+			shapeFactory.DeployShape(0);
+			shape = board.Shape;
+			shape.MoveDown();
+			shape.Rotate();
+			shape.MoveRight();
+			shape.MoveRight();
+			shape.MoveRight();
+			shape.Drop();
+
+			shapeFactory.DeployShape(0);
+			shape = board.Shape;
+			shape.MoveDown();
+			shape.Rotate();
+			shape.MoveRight();
+			shape.MoveRight();
+			shape.MoveRight();
+			shape.MoveRight();
+			shape.Drop();
+
+			//assert
+			Assert.AreEqual(800, score.score);
+			Assert.AreEqual(4, score.Lines);
+			Assert.AreEqual(4, score.Level);
+		}
     }
 }
